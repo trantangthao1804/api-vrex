7dc0ed7d2603d9445f74270293e3e159a16aea07
ggj <ggj@ggjs-MacBook-Air-3.local>
2023-05-28 13:04:15 +0700

    add config cros

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { configSwagger } from './configs/api-docs.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3200',
+      'https://VREX-DEMO.vercel.app/',
+    ],
+    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE","OPTIONS"],
+    credentials: true,
+  });
   configSwagger(app);
   await app.listen(3000);
 
