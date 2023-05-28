3cc3f1087e971642f73d68d4d0e0c85342aa7d49
ggj <ggj@ggjs-MacBook-Air-3.local>
2023-05-28 12:43:10 +0700

    config swagger

--- src/configs/api-docs.config.ts
+++ src/configs/api-docs.config.ts
@@ -7,5 +7,17 @@ export function configSwagger(app: INestApplication) {
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+    SwaggerModule.setup('api-docs', app, document,
+        {
+            customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+            customJs: [
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+            ],
+            customCssUrl: [
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+                'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+            ],
+        });
 }
